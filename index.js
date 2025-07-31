b5872530d27321f2b80229d1c2258ecb1f0fc752
Marco Bacci <marcobacci94@gmail.com>
2025-07-31 16:43:33 +0200

    feat: import list from another file and complete exercise

--- index.js
+++ index.js
@@ -1,58 +1,25 @@
+
 // importo express
-const express = require ('express')
+const express = require("express");
 // chiamo la funzione express e ne restituisco il valore in una variabile
-const app = express()
+const app = express();
 // chiamo la porta 3000
-const port = 3000
+const port = 3000;
 
 // definisco la rotta base
-app.get('/', (req, res)=>{
-  res.send('Server del mio blog')
-})
+app.get("/", (req, res) => {
+  res.send("Server del mio blog");
+});
 
 // insersico middleware per file statici
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/bacheca', (req, res)=>{
-  const movies = [
-    {
-      titolo:"Il Padrino",
-      contenuto:"La saga mafiosa più iconica della storia del cinema",
-      immagine:"/img/padrino.jpg",
-      tags:["drammatico", "mafia", "classico", "anni '70"],
-    },
-    {
-      titolo:"Inception",
-      contenuto:"Un labirinto mentale dove il confine tra realtà e sogno si fa sottile",
-      immagine:"/img/inception.jpg",
-      tags:["fantascienza", "thriller", "azione", "mind-bending"],
-    },
-    {
-      titolo:"Lock & Stock",
-      contenuto:"Un intreccio brillante di truffe, gangster e humour britannico in stile pulp",
-      immagine:"/img/lockstock.jpg",
-      tags:["crime", "commedia", "britannico", "anni '90", "cult"],
-    },
-    {
-      titolo:"Il Signore degli Anelli",
-      contenuto:"Magia, amicizia e coraggio si intrecciano in una delle saghe fantasy più amate di sempre",
-      immagine:"/img/ilsignoredeglianelli.jpg",
-      tags:["fantasy", "avventura", "epico", "trilogia"],
-    },
-    {
-      titolo:"Parasite",
-      contenuto:"Una satira sociale pungente che mescola dramma, suspense e colpi di scena",
-      immagine:"/img/parasite.jpg",
-      tags:["thriller", "drammatico", "coreano", "sociale"],
-    }
-  ];
-  res.json(movies)
-})
-
-
-
+app.get("/bacheca", (req, res) => {
+  const movies = require('./movies')
+  res.json(movies);
+});
 
 // dico al server di rimanere in ascolto sulla porta
-app.listen(port, ()=>{
-  console.log(`Server del blog in ascolto sulla porta ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server del blog in ascolto sulla porta ${port}`);
+});
---
+++ movies.js
@@ -0,0 +1,37 @@
+const movies = [
+    {
+      titolo: "Il Padrino",
+      contenuto: "La saga mafiosa più iconica della storia del cinema",
+      immagine: "/img/padrino.jpg",
+      tags: ["drammatico", "mafia", "classico", "anni '70"],
+    },
+    {
+      titolo: "Inception",
+      contenuto:
+        "Un labirinto mentale dove il confine tra realtà e sogno si fa sottile",
+      immagine: "/img/inception.jpg",
+      tags: ["fantascienza", "thriller", "azione", "mind-bending"],
+    },
+    {
+      titolo: "Lock & Stock",
+      contenuto:
+        "Un intreccio brillante di truffe, gangster e humour britannico in stile pulp",
+      immagine: "/img/lockstock.jpg",
+      tags: ["crime", "commedia", "britannico", "anni '90", "cult"],
+    },
+    {
+      titolo: "Il Signore degli Anelli",
+      contenuto:
+        "Magia, amicizia e coraggio si intrecciano in una delle saghe fantasy più amate di sempre",
+      immagine: "/img/ilsignoredeglianelli.jpg",
+      tags: ["fantasy", "avventura", "epico", "trilogia"],
+    },
+    {
+      titolo: "Parasite",
+      contenuto:
+        "Una satira sociale pungente che mescola dramma, suspense e colpi di scena",
+      immagine: "/img/parasite.jpg",
+      tags: ["thriller", "drammatico", "coreano", "sociale"],
+    },
+  ];
+  module.exports = movies
