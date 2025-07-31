e2b76dde6c3d6e7aaa527f3dd41ea6a8561eac6b
Marco Bacci <marcobacci94@gmail.com>
2025-07-31 14:44:02 +0200

    feat: create route

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+// importo express
+const express = require ('express')
+// chiamo la funzione express e ne restituisco il valore in una variabile
+const app = express()
+// chiamo la porta 3000
+const port = 3000
+
+// definisco la rotta base
+app.get('/', (req, res)=>{
+  res.send('Server del mio blog')
+})
+// dico al server di rimanere in ascolto sulla porta
+app.listen(port, ()=>{
+  console.log(`Server del blog in ascolto sulla porta ${port}`)
+})
